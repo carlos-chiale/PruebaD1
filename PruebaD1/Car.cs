8a9885d348b39cfdc9b51018a19bef0370863880
Carlos Chiale <carlos.chiale@hotmail.com>
2019-07-05 09:24:55 -0300

    Creada clase Auto

---
+++ PruebaD1/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaD1
+{
+    public class Auto
+    {
+        String name { get; set; }
+
+        String model { get; set; }
+
+        DateTime dateOfCensus { get; set}
+
+    }
+}
